654dc131b85e1f5c5acd9fadca5c114eee1f9c73
ssh <949589855@qq.com>
2021-05-28 19:04:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ MD_HEAD = """## ssh 的博客
 
 博客网站（不定期同步）：https://ssh-blog.vercel.app
 
-欢迎找我交个朋友，微信：**ssusunlight**，我喜欢分享前端的前沿趋势和一些有趣的事情。
+我是 ssh，欢迎找我交个朋友，微信：**[sshsunlight](https://images.gitee.com/uploads/images/2021/0528/190352_ae7dcd60_1087321.png)**，我喜欢分享前端的前沿趋势和一些有趣的事情。
 
-关注公众号「[前端从进阶到入院](https://user-gold-cdn.xitu.io/2020/4/5/17149ccf687b7699?w=910&h=436&f=jpeg&s=78195)」，精选超级优质的文章，助你前端成神之路。
+关注公众号「**[前端从进阶到入院](https://user-gold-cdn.xitu.io/2020/4/5/17149ccf687b7699?w=910&h=436&f=jpeg&s=78195)**」，精选超级优质的文章，助你前端成神之路。
 
 Thanks to https://github.com/yihong0618/gitblog
 """
